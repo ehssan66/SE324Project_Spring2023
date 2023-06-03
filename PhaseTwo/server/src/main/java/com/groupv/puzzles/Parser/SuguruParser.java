3d3dccaa0e42e695394e859da042363725e91a41
Ala Eddin A. Aldoukhi <75493673+Alaeddin03@users.noreply.github.com>
2023-06-03 12:36:27 +0300

    suguru parser

---
+++ PhaseTwo/server/src/main/java/com/groupv/puzzles/Parser/SuguruParser.java
@@ -0,0 +1,45 @@
+package com.groupv.puzzles.Parser;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import com.groupv.puzzles.Puzzle.ParsedPuzzle;
+
+public class SuguruParser implements PuzzleParser {
+
+    /**
+     * Parses the given file into a Puzzle object.
+     *
+     * @param path the path to the file to parse
+     * @return the Puzzle object
+     * @throws IOException if the file cannot be read
+     * @throws URISyntaxException if the file path is invalid
+     */
+    @Override
+    public ParsedPuzzle<String> parse(String fileName) throws IOException, URISyntaxException {
+
+        URI uri = new URI(InkyParser.class.getClassLoader().getResource("static/public/" + fileName).toString());
+        String path = uri.getPath();
+        BufferedReader reader = new BufferedReader(new FileReader(path));
+
+        String line = reader.readLine();
+        int size = Integer.parseInt(line);
+        String[][] grid = new String[size][size];
+        ParsedPuzzle<String> puzzle = new ParsedPuzzle<String>(size, grid);
+
+        for (int i = 0; i < size; i++) {
+            line = reader.readLine();
+            String[] tokens = line.split(" ");
+            for (int j = 0; j < size; j++) {
+                String value = tokens[j];
+                puzzle.setCell(i, j, value);
+            }
+        }
+
+        reader.close();
+        return puzzle;
+    }
+}
