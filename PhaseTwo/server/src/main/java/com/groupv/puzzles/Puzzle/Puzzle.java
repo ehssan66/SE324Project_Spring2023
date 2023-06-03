6e44277ede054ef94b6170cabd898f7e34925da0
Ala Eddin A. Aldoukhi <75493673+Alaeddin03@users.noreply.github.com>
2023-06-03 09:25:15 +0300

    added puzzle attributes (need checking)

--- PhaseTwo/server/src/main/java/com/groupv/puzzles/Puzzle/Puzzle.java
+++ PhaseTwo/server/src/main/java/com/groupv/puzzles/Puzzle/Puzzle.java
@@ -29,19 +29,70 @@ public class Puzzle {
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name="puzzle_type_id", nullable=false)
+    @JoinColumn(name = "puzzle_type_id", nullable = false)
     @RestResource(path = "type")
     private PuzzleType type;
 
     @Column(nullable = false)
     private String link;
 
-    @Column(nullable = false, updatable=false)
+    @Column(nullable = false, updatable = false)
     @CreationTimestamp
     private Date created_at;
 
     @ManyToMany(fetch = FetchType.LAZY, mappedBy = "puzzles")
     private List<Solution> solutions;
+
+    private int size;
+    private int[][] grid;
+
+    /**
+     * Constructs a new Puzzle with the given size and grid.
+     *
+     * @param size the size of the Puzzle
+     * @param grid the grid of the Puzzle
+     */
+    public Puzzle(int size, int[][] grid) {
+        this.size = size;
+        this.grid = grid;
+    }
+
+    /**
+     * Returns the size of the Puzzle.
+     * 
+     * @return the size of the Puzzle
+     */
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     * Sets the size of the Puzzle.
+     * 
+     * @param size the size of the Puzzle
+     */
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    /**
+     * Sets the value of the cell at the given row and column.
+     *
+     * @param row the row of the cell
+     * @param col the column of the cell
+     */
+    public void setCell(int row, int col, int value) {
+        grid[row][col] = value;
+    }
+
+    /**
+     * Returns the value of the grid.
+     *
+     * @return the value of the grid
+     */
+    public int[][] getGrid() {
+        return grid;
+    }
 
     /**
      * Constructs a new Puzzle with the given PuzzleType and link.
@@ -52,5 +103,16 @@ public class Puzzle {
     public Puzzle(PuzzleType type, String link) {
         this.type = type;
         this.link = link;
+    }
+
+    public String toString() {
+        String grid = "";
+        for (int i = 0; i < size; i++) {
+            grid += "\n";
+            for (int j = 0; j < size; j++) {
+                grid += this.grid[i][j] + " ";
+            }
+        }
+        return "Puzzle: \n" + "size: " + size + "\ngrid: " + grid;
     }
 }
