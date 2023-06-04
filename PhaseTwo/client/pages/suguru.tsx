ead8b4581b537e007cbc73ea008b2449ce386575
Atom <albaraa456@gmail.com>
2023-06-05 01:44:52 +0300

    conlicts solved, consistancy

--- PhaseTwo/client/pages/suguru.tsx
+++ PhaseTwo/client/pages/suguru.tsx
@@ -1,6 +1,5 @@
 import { Open_Sans } from 'next/font/google'
-import Game from '@/components/SuguruGame'
-import Game from '@/components/InkyGame'
+import SuguruGame from '@/components/SuguruGame'
 import Layout from '../components/layouts'
 
 const OpenSans = Open_Sans({ subsets: ['latin'], display: 'swap' })
@@ -9,7 +8,7 @@ export default function suguru() {
   return (
     <Layout>
       <main className="flex flex-col items-center w-full h-full flex-grow max-w-6xl mx-auto overflow-hidden">
-        <Game />
+        <SuguruGame />
       </main>
     </Layout>
   )
