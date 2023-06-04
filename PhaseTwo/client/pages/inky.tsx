71adf6b0d8d4737330462f05dd5758fc4bca1692
my name <heybassant@gmail.com>
2023-06-04 23:54:06 +0300

    Berry's FrontTouches

---
+++ PhaseTwo/client/pages/inky.tsx
@@ -0,0 +1,15 @@
+import { Open_Sans } from 'next/font/google'
+import Game from '@/components/Game'
+import Layout from '../components/layouts'
+
+const OpenSans = Open_Sans({ subsets: ['latin'], display: 'swap' })
+
+export default function inky() {
+  return (
+    <Layout>
+      <main className="flex flex-col items-center w-full h-full flex-grow max-w-6xl mx-auto overflow-hidden">
+        <Game />
+      </main>
+    </Layout>
+  )
+}
